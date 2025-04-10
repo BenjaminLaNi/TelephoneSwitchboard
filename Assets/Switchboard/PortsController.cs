a84e7642d4cecbf8cdd5b33fe248a7d42bf840b6
BenjaminLaNi <benjamin@onerepair.dk>
2025-04-10 10:02:27 +0200

    Help me

--- Assets/Switchboard/PortsController.cs
+++ Assets/Switchboard/PortsController.cs
@@ -5,14 +5,18 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PortsController : MonoBehaviour
 {
+    public Dictionary<PortController, Action> incomingCalls = new Dictionary<PortController, Action>();
+    public List<Call> currentCalls = new List<Call>();
+
     List<PortController> ports;
     public Dictionary<string, List<CableJackController>> cables;
     public GameObject cablesParent;
-    public string[] requiredConnection = new string[] { "", "" };
+    public PortController[] requiredConnection = new PortController[] { };
     public string requiredOperatorId = "";
     public Action requiredOperatorCallback = new Action(() => { });
     // Start is called before the first frame update
@@ -33,20 +37,22 @@ public class PortsController : MonoBehaviour
         {
             port.OccupiedCallback = (isOccupied) =>
             {
-                if (requiredOperatorId == port.PortName && isOccupied && port.IsOperatorCable)
+                if (!isOccupied) { return; }
+                if (incomingCalls.ContainsKey(port) && isOccupied && port.IsOperatorCable)
                 {
-                    requiredOperatorCallback();
-                    requiredOperatorId = "";
-                    requiredOperatorCallback = new Action(() => { });
+                    incomingCalls[port]();
+                    incomingCalls.Remove(port);
                     return;
                 }
-                if (requiredConnection.Contains(port.PortName) && isOccupied)
+                if (requiredConnection.Contains(port))
                 {
                     foreach (var cable in cables)
                     {
-                        if (requiredConnection.Contains(cable.Value[0].PortID) && requiredConnection.Contains(cable.Value[1].PortID))
+                        if (requiredConnection.Contains(cable.Value[0].connection) && requiredConnection.Contains(cable.Value[1].connection))
                         {
-                            Debug.Log("yipee " + cable.Key);
+
+                            StartCall(requiredConnection[0], requiredConnection[1]);
+                            requiredConnection = new PortController[] { };
                         }
                     }
                 }
@@ -61,16 +67,52 @@ public class PortsController : MonoBehaviour
 
     }
 
+    public void RequireConnection(PortController first, PortController second)
+    {
+        requiredConnection = new PortController[] { first, second };
+    }
+
     public void RequireConnection(string firstId, string secondId)
     {
-        requiredConnection = new string[] { firstId, secondId };
-        Debug.Log(firstId + " -> " + secondId);
+        RequireConnection(ports.Find(p => p.PortName == firstId), ports.Find(p => p.PortName == secondId));
     }
 
     public void RequireOperator(string id, Action callback)
     {
-        requiredOperatorId = id;
-        ports.Where((p) => p.PortName == id).FirstOrDefault().ToggleLight(true);
-        requiredOperatorCallback = callback;
+        PortController _p = ports.Where((p) => p.PortName == id).FirstOrDefault();
+        incomingCalls.Add(_p, callback);
+        _p.ToggleLight(true);
+        //_p.BlinkLight();
     }
+
+    public void StartCall(PortController caller, PortController callee)
+    {
+        Call call = new Call()
+        {
+            caller = caller,
+            callee = callee,
+            callerName = "",
+            calleeName = ""
+        };
+        currentCalls.Add(call);
+        callee.BlinkLight();
+
+        IEnumerator endCall(Call call, float delaySeconds)
+        {
+            yield return new WaitForSeconds(delaySeconds);
+            currentCalls.Remove(call);
+            call.caller.ToggleLight(false);
+            call.callee.ToggleLight(false);
+        }
+
+        StartCoroutine(endCall(call, new System.Random().Next(10, 15)));
+    }
+}
+
+public class Call
+{
+    public PortController caller { get; set; }
+    public string callerName { get; set; }
+    public PortController callee { get; set; }
+    public string calleeName { get; set; }
 }
